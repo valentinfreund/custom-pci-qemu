4dcbf871423b82349aebcda262e6c4f498163201
valentinfreund <valentinof99@gmail.com>
2026-01-12 00:12:28 +0100

    Update drv_pci.c
    added misc device

--- drv_pci.c
+++ drv_pci.c
@@ -2,6 +2,7 @@
 #include <linux/pci.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/miscdevice.h>
 
 #define DRV_NAME "drv_pci"
 #define BUF_SIZE 256
@@ -21,10 +22,73 @@ MODULE_LICENSE("GPL");
 //############################
 // state container			##
 //############################
-struct mydev_state {
+struct dev_state {
     struct pci_dev *pdev;
     void __iomem *bar0;
+
+    char buffer[BUF_SIZE];
+    size_t buffer_len;
+
+    struct miscdevice miscdev;
 };
+
+//##################################################
+// @name 	    miscpci_read
+// @param		file, char, length, offset
+// @return		length of transferred data
+// @function	reads data from kernel space to 
+//              user space
+//-------------------------------------------------
+static ssize_t miscpci_read(struct file *file, char __user *ubuf, size_t len, loff_t *off)
+{
+    struct dev_state *st = container_of(file->private_data,
+                                          struct dev_state, miscdev);
+
+    if (*off >= st->buffer_len)
+        return 0; // EOF
+
+    if (len > st->buffer_len - *off)
+        len = st->buffer_len - *off;
+
+    if (copy_to_user(ubuf, st->buffer + *off, len))
+        return -EFAULT;
+
+    *off += len; //don't forget the offset
+    return len;
+}
+
+//##################################################
+// @name 	    miscpci_write
+// @param		file, char, length, offset
+// @return		length of copied data
+// @function	reads data from user space to 
+//              kernel space into buffer
+//-------------------------------------------------
+static ssize_t miscpci_write(struct file *file, const char __user *ubuf, size_t len, loff_t *off)
+{
+    struct dev_state *st = container_of(file->private_data,
+                                          struct dev_state, miscdev);
+
+    if (len > BUF_SIZE)
+        return -EINVAL;
+
+    if (copy_from_user(st->buffer, ubuf, len))
+        return -EFAULT;
+
+    st->buffer_len = len;
+    return len;
+}
+
+//############################
+// file operations    		##
+//############################
+static const struct file_operations miscpci_fops = {
+    .owner = THIS_MODULE,
+    .read  = miscpci_read,
+    .write = miscpci_write,
+};
+
+
 
 //##################################################
 // @name 	    custompci_probe
@@ -36,7 +100,7 @@ struct mydev_state {
 static int custompci_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 {
     int ret;
-    struct mydev_state *st;
+    struct dev_state *st;
 
     pr_info(DRV_NAME ": probe() called for device %04x:%04x\n",
             pdev->vendor, pdev->device);
@@ -66,10 +130,20 @@ static int custompci_probe(struct pci_dev *pdev, const struct pci_device_id *id)
         ret = -ENOMEM;
         goto err_release_region;
     }
-    
-    pr_info(DRV_NAME ": device has probed successfully");
+/* Setup misc device */
+    st->miscdev.minor = MISC_DYNAMIC_MINOR;
+    st->miscdev.name = DRV_NAME;
+    st->miscdev.fops = &mymiscpci_fops;
+
+    ret = misc_register(&st->miscdev);
+    if (ret)
+        goto err_iounmap;
+
+    pr_info(DRV_NAME ": PCI device probed and miscdev registered\n");
     return 0;
 
+err_iounmap:
+    pci_iounmap(pdev, st->bar0);
 err_release_region:
     pci_release_region(pdev, 0);
 err_disable:
@@ -86,8 +160,10 @@ err_disable:
 //-------------------------------------------------
 static void custompci_remove(struct pci_dev *pdev)
 {
-    struct mydev_state *st = pci_get_drvdata(pdev);
+    struct dev_state *st = pci_get_drvdata(pdev);
 
+    misc_deregister(&st->miscdev);
+    
     if (st->bar0)
         pci_iounmap(pdev, st->bar0);
 
@@ -100,18 +176,18 @@ static void custompci_remove(struct pci_dev *pdev)
 //############################
 // pci device table			##
 //############################
-static const struct pci_device_id mypci_ids[] = {
+static const struct pci_device_id qemupci_ids[] = {
     { PCI_DEVICE(VENDOR_ID, DEVICE_ID) },
     { 0 }
 };
-MODULE_DEVICE_TABLE(pci, mypci_ids);
+MODULE_DEVICE_TABLE(pci, qemupci_ids);
 
 //############################
 // function mapping			##
 //############################
 static struct pci_driver custompci_driver = {
     .name = DRV_NAME,
-    .id_table = mypci_ids,
+    .id_table = qemupci_ids,
     .probe = custompci_probe,
     .remove = custompci_remove,
 };
